5c0338d09b8f74b0b0c622cf6dfc6d180ab81146
Aadi Poddar <aadipoddarmail@gmail.com>
2024-04-20 03:00:53 +0530

    Created Database

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+
+using ServerLibrary.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Starting
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ??
+        throw new InvalidOperationException("Sorry , your Connection is not found"));
+});
 
 var app = builder.Build();
 
---
+++ ServerLibrary/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BaseLibrary.Entities;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace ServerLibrary.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<GeneralDepartment> GeneralDepartments { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Branch> Branches { get; set; }
+        public DbSet<Town> Towns { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
